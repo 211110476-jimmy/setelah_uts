def08568edfecc15b66f05d51bb77f1c80ac1f25
kenny <211110404@students.mikroskil.ac.id>
2023-06-18 00:55:46 +0700

    added login dart file

---
+++ lib/pages/auth/loginScreen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:validators/validators.dart';
+import '../mainPage.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.clear();
+    super.dispose();
+  }
+
+  bool isEmailCorrect = false;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ), 
+        centerTitle: true,
+      ),
+      
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx7IBkCtYd6ulSfLfDL-aSF3rv6UfmWYxbSE823q36sPiQNVFFLatTFdGeUSnmJ4tUzlo&usqp=CAU'),
+                fit: BoxFit.cover,
+                opacity: 0.3)),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.network(
+                      'https://assets6.lottiefiles.com/packages/lf20_k9wsvzgd.json',
+                      animate: true,
+                      height: 120,
+                      width: 600),
+                  Text(
+                    'Log In Now',
+                    style: GoogleFonts.indieFlower(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Please login to continue using our app',
+                    style: GoogleFonts.indieFlower(
+                      textStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontWeight: FontWeight.w300,
+                          // height: 1.5,
+                          fontSize: 15),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: isEmailCorrect? 280 : 200,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, bottom: 20, top: 20),
+                          child: TextFormField(
+                            controller: _textEditingController,
+                            onChanged: (val) {
+                              setState(() {
+                                isEmailCorrect = isEmail(val);
+                              });
+                            },
+                            decoration: const InputDecoration(
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Colors.purple,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              labelText: "Email",
+                              hintText: 'your-email@domain.com',
+                              labelStyle: TextStyle(color: Colors.purple),
+                              // suffixIcon: IconButton(
+                              //     onPressed: () {},
+                              //     icon: Icon(Icons.close,
+                              //         color: Colors.purple))
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        isEmailCorrect
+                            ? ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0)),
+                                    backgroundColor: isEmailCorrect == false
+                                        ? Colors.red
+                                        : Colors.purple,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 131, vertical: 20)
+                                    // padding: EdgeInsets.only(
+                                    //     left: 120, right: 120, top: 20, bottom: 20),
+                                    ),
+                                child: const Text(
+                                  'Log In',
+                                  style: TextStyle(fontSize: 17),
+                                ))
+                            : Container(),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'You have\'t any account?',
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.6),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
