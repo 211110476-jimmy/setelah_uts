1207be08f5988759ce169389176423e01f8980af
Erwin Hariadi <211110443@mikroskil.students.ac.id>
2023-06-25 22:36:15 +0700

    menambahkan page reset password dengan OTP

--- lib/pages/auth/forgotPasswordScreen.dart
+++ lib/pages/auth/forgotPasswordScreen.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:projek_app/pages/auth/resetpassword.dart';
+import 'package:projek_app/pages/auth/resetPassword.dart';
 import 'package:validators/validators.dart';
 import 'otpScreen.dart';
 
@@ -14,15 +14,15 @@ class forgotPasswordScreen extends StatefulWidget {
   State<forgotPasswordScreen> createState() => _forgotPasswordScreen();
 }
 
-  // ignore: camel_case_types
-  class _forgotPasswordScreen extends State<forgotPasswordScreen> {
-    final TextEditingController _textEditingController = TextEditingController();
+// ignore: camel_case_types
+class _forgotPasswordScreen extends State<forgotPasswordScreen> {
+  final TextEditingController _textEditingController = TextEditingController();
 
-    @override
-    void dispose() {
-      _textEditingController.clear();
-      super.dispose();
-    }
+  @override
+  void dispose() {
+    _textEditingController.clear();
+    super.dispose();
+  }
 
   bool isEmailCorrect = false;
   final _formKey = GlobalKey<FormState>();
@@ -30,22 +30,20 @@ class forgotPasswordScreen extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Navigator.of(context).pop(),
-        ), 
+        ),
         centerTitle: true,
       ),
-
       body: Container(
         decoration: const BoxDecoration(
             // color: Colors.red.withOpacity(0.1),
             image: DecorationImage(
                 image: NetworkImage(
                     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShp2T_UoR8vXNZXfMhtxXPFvmDWmkUbVv3A40TYjcunag0pHFS_NMblOClDVvKLox4Atw&usqp=CAU'),
-                    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx7IBkCtYd6ulSfLfDL-aSF3rv6UfmWYxbSE823q36sPiQNVFFLatTFdGeUSnmJ4tUzlo&usqp=CAU'),
+                // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx7IBkCtYd6ulSfLfDL-aSF3rv6UfmWYxbSE823q36sPiQNVFFLatTFdGeUSnmJ4tUzlo&usqp=CAU'),
                 fit: BoxFit.cover,
                 opacity: 0.3)),
         child: SafeArea(
@@ -56,11 +54,8 @@ class forgotPasswordScreen extends StatefulWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Lottie.asset(
-                      'assets/images/forgotpass.json',
-                      animate: true,
-                      height: 200,
-                      width: 680),
+                  Lottie.asset('assets/images/forgotpass.json',
+                      animate: true, height: 200, width: 680),
                   // logo here
                   // Image.asset(
                   //   'assets/images/logo_new.png',
@@ -102,86 +97,83 @@ class forgotPasswordScreen extends StatefulWidget {
                               left: 20, right: 20, bottom: 20, top: 20),
                           child: Form(
                             key: _formKey,
-                          child: TextFormField(
-                            controller: _textEditingController,
-                            onChanged: (val) {
-                              setState(() {
-                                isEmailCorrect = isEmail(val);
-                              });
-                            },
-                            decoration: const InputDecoration(
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              prefixIcon: Icon(
-                                Icons.person,
-                                color: Colors.purple,
+                            child: TextFormField(
+                              controller: _textEditingController,
+                              onChanged: (val) {
+                                setState(() {
+                                  isEmailCorrect = isEmail(val);
+                                });
+                              },
+                              decoration: const InputDecoration(
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                prefixIcon: Icon(
+                                  Icons.person,
+                                  color: Colors.purple,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                labelText: "Email",
+                                hintText: 'your-email@domain.com',
+                                labelStyle: TextStyle(color: Colors.purple),
+                                // suffixIcon: IconButton(
+                                //     onPressed: () {},
+                                //     icon: Icon(Icons.close,
+                                //         color: Colors.purple))
                               ),
-                              filled: true,
-                              fillColor: Colors.white,
-                              labelText: "Email",
-                              hintText: 'your-email@domain.com',
-                              labelStyle: TextStyle(color: Colors.purple),
-                              // suffixIcon: IconButton(
-                              //     onPressed: () {},
-                              //     icon: Icon(Icons.close,
-                              //         color: Colors.purple))
-                            ),
-                            validator: (value) {
+                              validator: (value) {
                                 if (value!.isEmpty) {
                                   return 'Enter email!';
-                                }
-                                else if(!isEmailCorrect) {
+                                } else if (!isEmailCorrect) {
                                   return 'Enter valid email';
                                 }
                               },
+                            ),
                           ),
                         ),
-                      ),
-
                         const SizedBox(
                           height: 1,
                         ),
                         ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0)),
-                                    backgroundColor: Colors.red,
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 131, vertical: 20)
-                                    // padding: EdgeInsets.only(
-                                    //     left: 120, right: 120, top: 20, bottom: 20),
-                                    ),
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                backgroundColor: Colors.red,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 131, vertical: 20)
+                                // padding: EdgeInsets.only(
+                                //     left: 120, right: 120, top: 20, bottom: 20),
+                                ),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
                                         builder: (BuildContext context) =>
                                             const otpScreen()));
-                                            }
-                                },
-                                child: const Text(
-                                  'Forgot Password',
-                                  style: TextStyle(fontSize: 17),
-                                ))
-                            , Container(),
+                              }
+                            },
+                            child: const Text(
+                              'Forgot Password',
+                              style: TextStyle(fontSize: 17),
+                            )),
+                        Container(),
                       ],
                     ),
                   ),
-
                 ],
               ),
             ),
           ),
         ),
       ),
-    );;
+    );
+    ;
   }
 }
---
+++ lib/pages/auth/resetPassword.dart
@@ -0,0 +1,239 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'loginScreen.dart';
+
+class resetPassword extends StatefulWidget {
+  const resetPassword({Key? key}) : super(key: key);
+
+  @override
+  State<resetPassword> createState() => _resetPasswordState();
+}
+
+class _resetPasswordState extends State<resetPassword> {
+    final _password = GlobalKey<FormState>();
+    final _confirmpassword = GlobalKey<FormState>();
+    // ignore: prefer_typing_uninitialized_variables
+    var confirmPass;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ), 
+        centerTitle: true,
+      ),
+
+      body: Container(
+        decoration: const BoxDecoration(
+            // color: Colors.red.withOpacity(0.1),
+            image: DecorationImage(
+                image: NetworkImage(
+                    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShp2T_UoR8vXNZXfMhtxXPFvmDWmkUbVv3A40TYjcunag0pHFS_NMblOClDVvKLox4Atw&usqp=CAU',
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx7IBkCtYd6ulSfLfDL-aSF3rv6UfmWYxbSE823q36sPiQNVFFLatTFdGeUSnmJ4tUzlo&usqp=CAU'),
+                fit: BoxFit.cover,
+                opacity: 0.3)),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Lottie.asset(
+                  //     'assets/images/signup.json',
+                  //     animate: true,
+                  //     height: 150,
+                  //     width: 630),
+                  // logo here
+                  // Image.asset(
+                  //   'assets/images/logo_new.png',
+                  //   height: 120,
+                  //   width: 120,
+                  // ),
+                  Text(
+                    'Reset Password',
+                    style: GoogleFonts.indieFlower(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 30,
+                  ),
+
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      children: [
+
+                        //Password
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          child: Form(
+                            key: _password,
+                            child: TextFormField(
+                              obscuringCharacter: '*',
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                prefixIcon: Icon(
+                                  Icons.person,
+                                  color: Colors.purple,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                labelText: "Password",
+                                hintText: '*********',
+                                labelStyle: TextStyle(color: Colors.purple),
+                              ),
+                              validator: (value) {
+                                confirmPass = value;
+                                if (value!.isEmpty) {
+                                  return 'Enter a new password!';
+                                }
+                                else if(value.length <8){
+                                  return 'Password must have 8 character!';
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+
+                        //Reset Password
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          child: Form(
+                            key: _confirmpassword,
+                            child: TextFormField(
+                              obscuringCharacter: '*',
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                prefixIcon: Icon(
+                                  Icons.person,
+                                  color: Colors.purple,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                labelText: "Confirm Password",
+                                hintText: '*********',
+                                labelStyle: TextStyle(color: Colors.purple),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Enter a valid password!';
+                                }
+                                else if(value != confirmPass){
+                                  return 'Password doesn\'t match!';
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0)),
+                                    // backgroundColor: isEmailCorrect == false
+                                    //     ? Colors.red
+                                    //     : Colors.purple,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 131, vertical: 20)
+                                    // padding: EdgeInsets.only(
+                                    //     left: 120, right: 120, top: 20, bottom: 20),
+                                    ),
+                                onPressed: () {
+                                  if (_password.currentState!.validate() && _confirmpassword.currentState!.validate()) {
+                                    // If the form is valid, display a snackbar. In the real world,
+                                    // you'd often call a server or save the information in a database.
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text('Reset Success!')),
+                                    );
+                                  Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          const loginScreen()));
+                                  }
+                                  // Navigator.push(
+                                  //     context,
+                                  //     MaterialPageRoute(
+                                  //         builder: (context) => loginScreen()));
+                                },
+                                child: const Text(
+                                  'Reset Password',
+                                  style: TextStyle(fontSize: 17),
+                                ))
+                            , Container(),
+                      ],
+                    ),
+                  ),
+
+                  //this is button
+                  // const SizedBox(
+                  //   height: 30,
+                  // ),
+                  // ElevatedButton(
+                  //     style: ElevatedButton.styleFrom(
+                  //         shape: RoundedRectangleBorder(
+                  //             borderRadius: BorderRadius.circular(10.0)),
+                  //         backgroundColor: Colors.purple,
+                  //         padding: EdgeInsets.symmetric(
+                  //             horizontal: MediaQuery.of(context).size.width / 3.3,
+                  //             vertical: 20)
+                  //         // padding: EdgeInsets.only(
+                  //         //     left: 120, right: 120, top: 20, bottom: 20),
+                  //         ),
+                  //     onPressed: () {
+                  //       Navigator.push(
+                  //           context,
+                  //           MaterialPageRoute(
+                  //               builder: (context) => loginScreen()));
+                  //     },
+                  //     child: Text(
+                  //       'Sounds Good!',
+                  //       style: TextStyle(fontSize: 17),
+                  //     )), //
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
