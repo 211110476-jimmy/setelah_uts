a75d8979ede1e3c791ca7258f2aa7405eb32eaff
kenny <211110404@students.mikroskil.ac.id>
2023-07-11 20:42:21 +0700

    menambah conditional ketika shopping cart kosong

--- lib/pages/cartPage.dart
+++ lib/pages/cartPage.dart
@@ -11,58 +11,70 @@ class ShoppingCartPage extends StatelessWidget {
         backgroundColor: Colors.orange,
       ),
       body: Consumer<CartProvider>(
-        builder: (context, cartProvider, _) => ListView.builder(
-          itemCount: cartProvider.cartItems.length,
-          itemBuilder: (context, index) {
-            final cartItem = cartProvider.cartItems[index];
-            return ListTile(
-              leading: Image.network(cartItem.imagePath),
-              title: Text(
-                cartItem.name,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 15,
-                ),
-              ),
-              subtitle: Text(
-                'Rp${cartItem.price}',
-                style: const TextStyle(
-                  color: Colors.redAccent,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      if (cartItem.quantity > 1) {
-                        cartItem.quantity--;
-                        cartProvider.notifyListeners();
-                      }
-                    },
-                    icon: const Icon(Icons.remove),
-                  ),
-                  Text('${cartItem.quantity}'),
-                  IconButton(
-                    onPressed: () {
-                      cartItem.quantity++;
-                      cartProvider.notifyListeners();
-                    },
-                    icon: const Icon(Icons.add),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      cartProvider.removeFromCart(cartItem);
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                ],
-              ),
+        builder: (context, cartProvider, _) {
+          if (cartProvider.cartItems.length == 0) {
+            return Center(
+              child: Text("Nothing in the cart :(",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  )),
             );
-          },
-        ),
+          } else {
+            return ListView.builder(
+              itemCount: cartProvider.cartItems.length,
+              itemBuilder: (context, index) {
+                final cartItem = cartProvider.cartItems[index];
+                return ListTile(
+                  leading: Image.asset(cartItem.imagePath),
+                  title: Text(
+                    cartItem.name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Rp${cartItem.price}',
+                    style: const TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          if (cartItem.quantity > 1) {
+                            cartItem.quantity--;
+                            cartProvider.notifyListeners();
+                          }
+                        },
+                        icon: const Icon(Icons.remove),
+                      ),
+                      Text('${cartItem.quantity}'),
+                      IconButton(
+                        onPressed: () {
+                          cartItem.quantity++;
+                          cartProvider.notifyListeners();
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          cartProvider.removeFromCart(cartItem);
+                        },
+                        icon: const Icon(Icons.close),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(20.0),
